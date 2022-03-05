4173d2d62f4f34d9904c63d875ad038b41deb7a1
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-03-05 17:40:14 +1300

    Remove Twitch API dependency. Only uses discord.js now

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const prefix = config.prefix;
 var serverID = config.serverID;
 var channelID = config.channelID;
 var logChannelID = config.logChannelID;
-var _streamer = config.streamer;
-var checkTime = config.checkTime;
 
 var firstPass = true;
 var isLive;
@@ -18,7 +16,7 @@ var logChannel;
 var server;
 
 //Repeat tasks
-setInterval(() => getStreamInfo(_streamer), checkTime);
+//setInterval(() => getStreamInfo(_streamer), checkTime);
 
 //Login to DiscordAPI
 client.login(config.token);
@@ -49,8 +47,7 @@ client.on("message", function(message) {
 
     //Check if message has command prefix
     if (!message.content.startsWith(prefix)) return;
-    console.log("Command recieved");
-    logChannel.send("Command recieved");
+    console.log("Command received");
 
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
@@ -62,77 +59,31 @@ client.on("message", function(message) {
     }
 
     else{
-        message.reply(`Command not found`);
         return;
     }
 });
 
-// Get/output the streamer's details
-function getStreamInfo(streamer){
-    // Make the streamer/program globally accessible
-    _streamer = streamer;
-
-    // Store the header to use for all requests
-    const header = {
-        'Client-ID': '3zzmx0l2ph50anf78iefr6su9d8byj8',
-        'Accept':    'application/vnd.twitchtv.v5+json'
-    };
-
-    // Store the URL to request users...
-    const usersURL = 'https://api.twitch.tv/kraken/users?login=' + streamer
-    // ... and make the request
-    request.get(usersURL).set(header).end((err, res) => {
-        // If an error occured, log a failure
-        if (err) return failure(err);
-
-        // Next, get the users array from the response body...
-        const { users } = res.body;
-
-        // Next, get the user's ID...
-        const { _id: id } = users[0],
-            // ... store the URL to request a stream,...
-            streamsURL = 'https://api.twitch.tv/kraken/streams/' + id;
-        // ... and make the final request
-        request.get(streamsURL).set(header).end(response);
+//Check if Marshy is live
+client.on("presenceUpdate", (oldPresence, newPresence) => {
+    if(newPresence.user.tag == "MMarshyellow#2705") return;
+    if (!newPresence.activities) return false;
+    newPresence.activities.forEach(activity => {
+        if (activity.type == "STREAMING") {
+            isLive = true;
+            if(!isLocked || firstPass){
+                lock();
+            }
+            return;
+        }
+        else{
+            isLive = false;
+            if(isLocked || firstPass){
+                unlock();
+            }
+            return;
+        }
     });
-};
-
-// Handle the request response
-function response(error, response) {
-    return error ? failure(error) : success(response);
-}
-
-// Handle request success
-function success(response) {
-    // Attempt to get stream information from the response
-    stream = JSON.parse(response.text).stream;
-    // If there's no stream, let the user know the streamer is not streaming
-    if (!stream) {
-        isLive = false;
-        if(isLocked || firstPass){
-            unlock();
-        }
-        return;
-    }
-    if (stream) {
-        isLive = true;
-        if(!isLocked || firstPass){
-            lock();
-        }
-        return;
-    }
-    else {
-        console.log("Stream didnt return live status correctly");
-        logChannel.send("Stream didnt return live status correctly");
-    }
-}
-
-// Handle request failure
-function failure(error) {
-    console.log("An error occured!: " + error);
-    logChannel.send("An error occured!: " + error);
-    return process.exit(1);
-}
+});
 
 //Lock the discord channel
 function lock(){
